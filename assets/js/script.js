8dfb47cdaf19c41cf2637fe210e8c431c0a6ff36
Mousin Ali <mousinalikhan1234@gmail.com>
2025-10-15 10:04:58 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+    $('.fade-slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true, // Enables fade transition
+        cssEase: 'linear' // Optional: smoother fading
+    });
+
+    $('.test-slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+
+    $('.gallery-slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        variableWidth: false,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                },
+            },
+        ],
+    });
+});
+
+
+
+
