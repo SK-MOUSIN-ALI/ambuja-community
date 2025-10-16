f3ef7badad7dae0efa81ea5bfe128c8b07d0e75d
Mousin Ali <mousinalikhan1234@gmail.com>
2025-10-16 08:47:12 +0530

    dashboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,60 @@
-$(document).ready(function () {
-    $('.fade-slider').slick({
-        dots: false,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        fade: true, // Enables fade transition
-        cssEase: 'linear' // Optional: smoother fading
-    });
-
-    $('.test-slider').slick({
-        dots: false,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000
-    });
-
-    $('.gallery-slider').slick({
-        dots: false,
-        arrows: true,
-        infinite: true,
-        speed: 3000,
-        slidesToShow: 4,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        variableWidth: false,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 800,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: false,
-                },
-            },
-        ],
-    });
-});
-
-
-
-
+$('#offersScroll ').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    dots: false,
+    // autoplay: true,
+    // autoplaySpeed: 2500,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: { slidesToShow: 3 }
+        },
+        {
+            breakpoint: 992,
+            settings: { slidesToShow: 2 }
+        },
+        {
+            breakpoint: 576,
+            settings: { slidesToShow: 1 }
+        }
+    ]
+});
+
+$('#offers-prev').on('click', function () {
+    $('#offersScroll').slick('slickPrev');
+});
+$('#offers-next').on('click', function () {
+    $('#offersScroll').slick('slickNext');
+});
+$('#activitiesScroll ').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    dots: false,
+    // autoplay: true,
+    // autoplaySpeed: 2500,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: { slidesToShow: 3 }
+        },
+        {
+            breakpoint: 992,
+            settings: { slidesToShow: 2 }
+        },
+        {
+            breakpoint: 576,
+            settings: { slidesToShow: 1 }
+        }
+    ]
+});
+
+$('#activities-prev').on('click', function () {
+    $('#activitiesScroll').slick('slickPrev');
+});
+$('#activities-next').on('click', function () {
+    $('#activitiesScroll').slick('slickNext');
+});
